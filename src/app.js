c05c141cefce2e81c2fb4981d84bf03725823e1c
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-19 12:01:49 -0300

    node

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+const livros = [
+   {id: 1, "titulo": "Senhor dos Aneis"},
+   {id: 2, "titulo": "O Hobbit" }
+]
+
+app.get('/', (req, res) => {
+   res.status(200).send('Curso de Node');
+})
+
+app.get('/livros', (req, res) => {
+   res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+   livros.push(req.body);
+   res.status(201).send('Livro foi cadastrado com sucesso');
+})
+
+export default app;
